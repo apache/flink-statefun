1e9b6c2bee49bd41ef2769da02033234f750b1b7
Igal Shilman <igalshilman@gmail.com>
2019-12-05 15:17:10 +0100

    Add TemplateEvaluator
    This commit adds an evaluator for a template string based on a
    ProtobufPath string.

---
+++ stateful-functions-flink/stateful-functions-flink-core/src/main/java/com/ververica/statefun/flink/core/protorouter/TemplateEvaluator.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019 Ververica GmbH.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ververica.statefun.flink.core.protorouter;
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import com.ververica.statefun.flink.core.types.protobuf.protopath.ProtobufPath;
+import java.util.List;
+import java.util.function.Function;
+
+final class TemplateEvaluator {
+
+  private interface FragmentEvaluator {
+    void eval(StringBuilder builder, DynamicMessage message);
+  }
+
+  private final FragmentEvaluator[] fragmentEvaluators;
+  private final StringBuilder builder = new StringBuilder();
+
+  TemplateEvaluator(
+      Descriptors.Descriptor descriptor, List<TemplateParser.TextFragment> fragments) {
+    this.fragmentEvaluators = fragmentEvaluators(descriptor, fragments);
+  }
+
+  public String evaluate(DynamicMessage message) {
+    for (FragmentEvaluator e : fragmentEvaluators) {
+      e.eval(builder, message);
+    }
+    final String result = builder.toString();
+    builder.delete(0, builder.length());
+    return result;
+  }
+
+  private static FragmentEvaluator[] fragmentEvaluators(
+      Descriptors.Descriptor descriptor, List<TemplateParser.TextFragment> fragments) {
+    return fragments.stream()
+        .map(
+            fragment ->
+                fragment.dynamic()
+                    ? dynamicEvaluator(descriptor, fragment)
+                    : staticEvaluator(fragment))
+        .toArray(FragmentEvaluator[]::new);
+  }
+
+  private static FragmentEvaluator staticEvaluator(TemplateParser.TextFragment fragment) {
+    final String text = fragment.fragment();
+    return (builder, unused) -> builder.append(text);
+  }
+
+  private static FragmentEvaluator dynamicEvaluator(
+      Descriptors.Descriptor descriptor, TemplateParser.TextFragment fragment) {
+    final Function<DynamicMessage, ?> protopathEvaluator =
+        ProtobufPath.protobufPath(descriptor, fragment.fragment());
+    return (builder, message) -> {
+      Object result = protopathEvaluator.apply(message);
+      builder.append(result);
+    };
+  }
+}
