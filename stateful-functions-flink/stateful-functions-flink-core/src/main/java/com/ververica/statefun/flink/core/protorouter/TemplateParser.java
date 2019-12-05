72f0cec107504deba446ab989e304fbbeebc907a
Igal Shilman <igalshilman@gmail.com>
2019-12-05 16:27:14 +0100

    Make the class package private

--- stateful-functions-flink/stateful-functions-flink-core/src/main/java/com/ververica/statefun/flink/core/protorouter/TemplateParser.java
+++ stateful-functions-flink/stateful-functions-flink-core/src/main/java/com/ververica/statefun/flink/core/protorouter/TemplateParser.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class TemplateParser {
+final class TemplateParser {
   private static final Pattern DYNAMIC_FRAGMENT_PATTERN = Pattern.compile("\\{\\{([^}]*)}}");
 
   private TemplateParser() {}
