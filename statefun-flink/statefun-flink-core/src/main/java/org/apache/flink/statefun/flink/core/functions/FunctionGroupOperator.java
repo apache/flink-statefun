c7ba672d47ace11453e47ce9e04f00fd7f0d4e16
Igal Shilman <igalshilman@gmail.com>
2020-02-20 23:03:37 +0100

    [FLINK-16063][core] Disable global backpressure
    This commit disables global (per StreamTask) backpressure

--- statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/FunctionGroupOperator.java
+++ statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/FunctionGroupOperator.java
@@ -102,7 +102,7 @@ public class FunctionGroupOperator extends AbstractStreamOperator<Message>
     Objects.requireNonNull(mailboxExecutor, "MailboxExecutor is unexpectedly NULL");
 
     // TODO: once FLINK-16149 would be merged, we should pass the threshold as a configuration.
-    this.backPressureValve = new ThresholdBackPressureValve(1_000);
+    this.backPressureValve = new ThresholdBackPressureValve(-1);
 
     //
     // the core logic of applying messages to functions.
