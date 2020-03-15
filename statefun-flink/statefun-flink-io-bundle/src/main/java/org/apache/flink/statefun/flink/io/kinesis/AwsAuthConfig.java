5233c073c6b12a94693d323f10f810e4c4069649
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-03-16 00:05:06 +0800

    [FLINK-16124] [kinesis] Implement runtime KinesisSourceProvider

---
+++ statefun-flink/statefun-flink-io-bundle/src/main/java/org/apache/flink/statefun/flink/io/kinesis/AwsAuthConfig.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.statefun.flink.io.kinesis;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Properties;
+import org.apache.flink.kinesis.shaded.com.amazonaws.regions.DefaultAwsRegionProviderChain;
+import org.apache.flink.statefun.sdk.kinesis.auth.AwsCredentials;
+import org.apache.flink.statefun.sdk.kinesis.auth.AwsRegion;
+import org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants;
+
+final class AwsAuthConfig {
+  private final AwsRegion awsRegion;
+  private final AwsCredentials awsCredentials;
+
+  AwsAuthConfig(AwsRegion awsRegion, AwsCredentials awsCredentials) {
+    this.awsRegion = Objects.requireNonNull(awsRegion);
+    this.awsCredentials = Objects.requireNonNull(awsCredentials);
+  }
+
+  Properties asFlinkConnectorProperties() {
+    final Properties properties = new Properties();
+    setAwsRegionProperties(properties, awsRegion);
+    setAwsCredentialProperties(properties, awsCredentials);
+
+    return properties;
+  }
+
+  private static void setAwsRegionProperties(Properties properties, AwsRegion awsRegion) {
+    if (awsRegion.isDefault()) {
+      properties.setProperty(AWSConfigConstants.AWS_REGION, regionFromDefaultProviderChain());
+    } else if (awsRegion.isId()) {
+      properties.setProperty(AWSConfigConstants.AWS_REGION, awsRegion.asId().id());
+    } else if (awsRegion.isCustomEndpoint()) {
+      final AwsRegion.CustomEndpointAwsRegion customEndpoint = awsRegion.asCustomEndpoint();
+      properties.setProperty(AWSConfigConstants.AWS_ENDPOINT, customEndpoint.serviceEndpoint());
+      properties.setProperty(AWSConfigConstants.AWS_REGION, customEndpoint.regionId());
+    } else {
+      throw new IllegalStateException("Unrecognized AWS region configuration type: " + awsRegion);
+    }
+  }
+
+  private static void setAwsCredentialProperties(
+      Properties properties, AwsCredentials awsCredentials) {
+    if (awsCredentials.isDefault()) {
+      properties.setProperty(
+          AWSConfigConstants.AWS_CREDENTIALS_PROVIDER,
+          AWSConfigConstants.CredentialProvider.AUTO.name());
+    } else if (awsCredentials.isBasic()) {
+      properties.setProperty(
+          AWSConfigConstants.AWS_CREDENTIALS_PROVIDER,
+          AWSConfigConstants.CredentialProvider.BASIC.name());
+
+      final AwsCredentials.BasicAwsCredentials basicCredentials = awsCredentials.asBasic();
+      properties.setProperty(
+          AWSConfigConstants.accessKeyId(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER),
+          basicCredentials.accessKeyId());
+      properties.setProperty(
+          AWSConfigConstants.secretKey(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER),
+          basicCredentials.secretAccessKey());
+    } else if (awsCredentials.isProfile()) {
+      properties.setProperty(
+          AWSConfigConstants.AWS_CREDENTIALS_PROVIDER,
+          AWSConfigConstants.CredentialProvider.PROFILE.name());
+
+      final AwsCredentials.ProfileAwsCredentials profileCredentials = awsCredentials.asProfile();
+      properties.setProperty(
+          AWSConfigConstants.profileName(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER),
+          profileCredentials.name());
+      profileCredentials
+          .path()
+          .ifPresent(
+              path ->
+                  properties.setProperty(
+                      AWSConfigConstants.profilePath(AWSConfigConstants.AWS_CREDENTIALS_PROVIDER),
+                      path));
+    } else {
+      throw new IllegalStateException(
+          "Unrecognized AWS credentials configuration type: " + awsCredentials);
+    }
+  }
+
+  private static String regionFromDefaultProviderChain() {
+    return new DefaultAwsRegionProviderChain().getRegion().toLowerCase(Locale.ENGLISH);
+  }
+}
---
+++ statefun-flink/statefun-flink-io-bundle/src/main/java/org/apache/flink/statefun/flink/io/kinesis/KinesisSourceProvider.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.statefun.flink.io.kinesis;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+import org.apache.flink.statefun.flink.io.common.ReflectionUtil;
+import org.apache.flink.statefun.flink.io.spi.SourceProvider;
+import org.apache.flink.statefun.sdk.io.IngressSpec;
+import org.apache.flink.statefun.sdk.kinesis.ingress.KinesisIngressDeserializer;
+import org.apache.flink.statefun.sdk.kinesis.ingress.KinesisIngressSpec;
+import org.apache.flink.statefun.sdk.kinesis.ingress.KinesisIngressStartupPosition;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
+import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
+import org.apache.flink.streaming.connectors.kinesis.serialization.KinesisDeserializationSchema;
+import org.apache.flink.streaming.connectors.kinesis.util.AWSUtil;
+
+final class KinesisSourceProvider implements SourceProvider {
+
+  @Override
+  public <T> SourceFunction<T> forSpec(IngressSpec<T> spec) {
+    final KinesisIngressSpec<T> kinesisIngressSpec = asKinesisSpec(spec);
+
+    return new FlinkKinesisConsumer<>(
+        kinesisIngressSpec.streams(),
+        deserializationSchemaFromSpec(kinesisIngressSpec),
+        propertiesFromSpec(kinesisIngressSpec));
+  }
+
+  private static <T> KinesisIngressSpec<T> asKinesisSpec(IngressSpec<T> spec) {
+    if (spec instanceof KinesisIngressSpec) {
+      return (KinesisIngressSpec<T>) spec;
+    }
+    if (spec == null) {
+      throw new NullPointerException("Unable to translate a NULL spec");
+    }
+    throw new IllegalArgumentException(String.format("Wrong type %s", spec.type()));
+  }
+
+  private static <T> KinesisDeserializationSchema<T> deserializationSchemaFromSpec(
+      KinesisIngressSpec<T> spec) {
+    KinesisIngressDeserializer<T> ingressDeserializer =
+        ReflectionUtil.instantiate(spec.deserializerClass());
+    return new KinesisDeserializationSchemaDelegate<>(ingressDeserializer);
+  }
+
+  private static Properties propertiesFromSpec(KinesisIngressSpec<?> spec) {
+    final Properties properties = new Properties();
+
+    properties.putAll(resolveClientProperties(spec.clientConfigurationProperties()));
+
+    final AwsAuthConfig awsAuthConfig = new AwsAuthConfig(spec.awsRegion(), spec.awsCredentials());
+    properties.putAll(awsAuthConfig.asFlinkConnectorProperties());
+
+    setStartupPositionProperties(properties, spec.startupPosition());
+
+    return properties;
+  }
+
+  private static Properties resolveClientProperties(Properties clientConfigurationProperties) {
+    final Properties resolvedProps = new Properties();
+    for (String property : clientConfigurationProperties.stringPropertyNames()) {
+      resolvedProps.setProperty(
+          asFlinkConsumerClientPropertyKey(property),
+          clientConfigurationProperties.getProperty(property));
+    }
+    return resolvedProps;
+  }
+
+  private static void setStartupPositionProperties(
+      Properties properties, KinesisIngressStartupPosition startupPosition) {
+    if (startupPosition.isEarliest()) {
+      properties.setProperty(
+          ConsumerConfigConstants.STREAM_INITIAL_POSITION,
+          ConsumerConfigConstants.InitialPosition.TRIM_HORIZON.name());
+    } else if (startupPosition.isLatest()) {
+      properties.setProperty(
+          ConsumerConfigConstants.STREAM_INITIAL_POSITION,
+          ConsumerConfigConstants.InitialPosition.LATEST.name());
+    } else if (startupPosition.isDate()) {
+      properties.setProperty(
+          ConsumerConfigConstants.STREAM_INITIAL_POSITION,
+          ConsumerConfigConstants.InitialPosition.AT_TIMESTAMP.name());
+
+      final ZonedDateTime startupDate = startupPosition.asDate().date();
+      final DateTimeFormatter formatter =
+          DateTimeFormatter.ofPattern(ConsumerConfigConstants.DEFAULT_STREAM_TIMESTAMP_DATE_FORMAT);
+      properties.setProperty(
+          ConsumerConfigConstants.STREAM_INITIAL_TIMESTAMP, startupDate.format(formatter));
+    } else {
+      throw new IllegalStateException(
+          "Unrecognized ingress startup position type: " + startupPosition);
+    }
+  }
+
+  private static String asFlinkConsumerClientPropertyKey(String key) {
+    return AWSUtil.AWS_CLIENT_CONFIG_PREFIX + lowercaseFirstLetter(key);
+  }
+
+  private static String lowercaseFirstLetter(String string) {
+    final char[] chars = string.toCharArray();
+    chars[0] = Character.toLowerCase(chars[0]);
+    return new String(chars);
+  }
+}
