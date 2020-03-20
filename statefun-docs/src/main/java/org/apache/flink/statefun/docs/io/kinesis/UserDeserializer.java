33b2f8912709de4bf0659901b93dcc29d06c6a31
Seth Wiesman <sjwiesman@gmail.com>
2020-03-20 10:38:23 -0500

    [FLINK-16700][docs] Document Kinesis I/O Module

---
+++ statefun-docs/src/main/java/org/apache/flink/statefun/docs/io/kinesis/UserDeserializer.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.flink.statefun.docs.io.kinesis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.apache.flink.statefun.docs.models.User;
+import org.apache.flink.statefun.sdk.kinesis.ingress.IngressRecord;
+import org.apache.flink.statefun.sdk.kinesis.ingress.KinesisIngressDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserDeserializer implements KinesisIngressDeserializer<User> {
+
+  private static Logger LOG = LoggerFactory.getLogger(UserDeserializer.class);
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @Override
+  public User deserialize(IngressRecord ingressRecord) {
+    try {
+      return mapper.readValue(ingressRecord.getData(), User.class);
+    } catch (IOException e) {
+      LOG.debug("Failed to deserialize record", e);
+      return null;
+    }
+  }
+}
