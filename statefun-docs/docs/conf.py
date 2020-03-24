6cbef161d582dc9b8a4b4fff388699f97230aea1
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-03-24 11:38:31 +0800

    [release] Update version to 2.1-SNAPSHOT

--- statefun-docs/docs/conf.py
+++ statefun-docs/docs/conf.py
@@ -64,9 +64,9 @@ html_show_copyright = False
 # built documents.
 #
 # The short X.Y version.
-version = u'2.0-SNAPSHOT'
+version = u'2.1-SNAPSHOT'
 # The full version, including alpha/beta/rc tags.
-release = u'2.0.0'
+release = u'2.1.0'
 # The latest Flink version supported by Stateful Functions
 flinkVersion = u'1.10'
 # The latest full Flink version supported by Stateful Functions
