99a654242b2a249a065ac71b985f5339f963fd1e
Igal Shilman <igalshilman@gmail.com>
2020-04-02 00:01:51 +0200

    [FLINK-16927][core] Add legacy scheduler property validation
    This commit requires that the value of `jobmanager.scheduler` configuration
    parameter is set to legacy.
    
    This closes #91

--- statefun-flink/statefun-flink-harness/src/main/java/org/apache/flink/statefun/flink/harness/Harness.java
+++ statefun-flink/statefun-flink-harness/src/main/java/org/apache/flink/statefun/flink/harness/Harness.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import java.util.Objects;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.CoreOptions;
+import org.apache.flink.configuration.JobManagerOptions;
+import org.apache.flink.runtime.dispatcher.SchedulerNGFactoryFactory;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsConfig;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsConfigValidator;
 import org.apache.flink.statefun.flink.core.StatefulFunctionsJob;
@@ -167,5 +169,6 @@ public class Harness {
     flinkConfig.set(
         ExecutionCheckpointingOptions.MAX_CONCURRENT_CHECKPOINTS,
         StatefulFunctionsConfigValidator.MAX_CONCURRENT_CHECKPOINTS);
+    flinkConfig.set(JobManagerOptions.SCHEDULER, SchedulerNGFactoryFactory.SCHEDULER_TYPE_LEGACY);
   }
 }
