c536942e6e076bd0a146e1b94e4faa20c7d7f538
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-04-08 13:48:39 +0800

    [hotfix] [build] Ignore license checks in snapshot deployments

--- tools/snapshots/deploy_snapshot_jars.sh
+++ tools/snapshots/deploy_snapshot_jars.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-################################################################################
-#  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
+
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
-################################################################################
+#
 
 #
 # Deploys snapshot builds to Apache's snapshot repository.
@@ -49,7 +49,7 @@ echo "Detected current version as: '$CURRENT_STATEFUN_VERSION'"
 
 if [[ ${CURRENT_STATEFUN_VERSION} == *SNAPSHOT* ]] ; then
     echo "Deploying to repository.apache.org/content/repositories/snapshots/"
-    ${MVN} clean deploy -Papache-release -Dgpg.skip -DskipTests -DretryFailedDeploymentCount=10
+    ${MVN} clean deploy -Papache-release -Dgpg.skip -Drat.skip=true -Drat.ignoreErrors=true -DskipTests -DretryFailedDeploymentCount=10
     exit 0
 else
     echo "Snapshot deployments should only be done for snapshot versions"
