3f30aad0e22f03715cc5083bd573ccdcd2b8a6b2
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-05-28 19:45:56 +0800

    [release] Update version to 2.2-SNAPSHOT

--- tools/docker/build-stateful-functions.sh
+++ tools/docker/build-stateful-functions.sh
@@ -21,7 +21,7 @@ set -e
 # Do not change the name of this variable;
 # it is referenced in the tools/releasing/update_branch_version.sh script
 #
-VERSION_TAG=2.1-SNAPSHOT
+VERSION_TAG=2.2-SNAPSHOT
 
 #
 # setup the environment 
