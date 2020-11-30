9b12dfe717281d6cffd188405aea192fdf5cf316
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-11-30 15:05:39 +0800

    [FLINK-20334] [core] Wire in namespace providers into function loaders

--- statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/PredefinedFunctionLoader.java
+++ statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/PredefinedFunctionLoader.java
@@ -29,26 +29,39 @@ import org.apache.flink.statefun.sdk.StatefulFunctionProvider;
 /** An {@link FunctionLoader} that has a predefined set of {@link StatefulFunctionProvider}s. */
 final class PredefinedFunctionLoader implements FunctionLoader {
   private final Map<FunctionType, StatefulFunctionProvider> functionProviders;
+  private final Map<String, StatefulFunctionProvider> namespaceFunctionProviders;
 
   @Inject
   PredefinedFunctionLoader(
-      @Label("function-providers") Map<FunctionType, StatefulFunctionProvider> functionProviders) {
+      @Label("function-providers") Map<FunctionType, StatefulFunctionProvider> functionProviders,
+      @Label("namespace-function-providers")
+          Map<String, StatefulFunctionProvider> namespaceFunctionProviders) {
     this.functionProviders = Objects.requireNonNull(functionProviders);
+    this.namespaceFunctionProviders = Objects.requireNonNull(namespaceFunctionProviders);
   }
 
   @Override
   public StatefulFunction load(FunctionType functionType) {
     Objects.requireNonNull(functionType);
-    StatefulFunctionProvider provider = functionProviders.get(functionType);
-    if (provider == null) {
-      throw new IllegalArgumentException("Unknown provider for type " + functionType);
-    }
-    StatefulFunction statefulFunction = load(provider, functionType);
+    final StatefulFunctionProvider provider = getFunctionProviderOrThrow(functionType);
+    final StatefulFunction statefulFunction = load(provider, functionType);
     if (statefulFunction == null) {
       throw new IllegalStateException(
           "A provider for a type " + functionType + " has produced a NULL function");
     }
     return statefulFunction;
+  }
+
+  private StatefulFunctionProvider getFunctionProviderOrThrow(FunctionType functionType) {
+    StatefulFunctionProvider provider = functionProviders.get(functionType);
+    if (provider != null) {
+      return provider;
+    }
+    provider = namespaceFunctionProviders.get(functionType.namespace());
+    if (provider != null) {
+      return provider;
+    }
+    throw new IllegalArgumentException("Cannot find a provider for type " + functionType);
   }
 
   private static StatefulFunction load(
