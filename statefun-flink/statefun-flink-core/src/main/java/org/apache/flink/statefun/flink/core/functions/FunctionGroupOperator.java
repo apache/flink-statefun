f3f7d994dc0ea476a524c8179cb2dbe70ae5c4c4
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-12-23 13:03:14 +0800

    [hotfix] isUsingCustomRawKeyedState should be set on FeedbackUnionOperator

--- statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/FunctionGroupOperator.java
+++ statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/functions/FunctionGroupOperator.java
@@ -151,11 +151,6 @@ public class FunctionGroupOperator extends AbstractStreamOperator<Message>
   }
 
   @Override
-  protected boolean isUsingCustomRawKeyedState() {
-    return true;
-  }
-
-  @Override
   public void close() throws Exception {
     try {
       closeOrDispose();
